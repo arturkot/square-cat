c4215194a792742a1b00b77a5e92d51d98ad18be
Artur Kot <arturkot85@gmail.com>
2022-03-07 16:49:35 +0100

    Create <square-cat> web component

--- main.js
+++ main.js
@@ -14,138 +14,174 @@ import { Mesh } from "three/src/objects/Mesh";
 import { GLTFLoader } from "./GLTFLoader.js";
 
 const ANIMATION_SPEED = 4;
-const cameraX = -200;
-const cameraY = 200;
-const cameraZ = -200;
+const CAMERA_X = -200;
+const CAMERA_Y = 200;
+const CAMERA_Z = -200;
 
-const isHoverSupport = matchMedia("(hover: hover").matches;
-let catCenterXpos = window.innerWidth / 2;
-let catCenterYpos = 280;
-let mouseXpos = false;
-let mouseYpos = false;
-let isAnimating = false;
-let startAngle = 0;
-let currentAngle = 0;
+const styles = new URL("./component-style.css", import.meta.url).href;
 
-const $container = document.getElementById("js-3dcat");
-
-$container.classList.add("threeActive");
-const w = $container.offsetWidth * 2;
-const h = $container.offsetHeight * 2;
-const scene = new Scene();
-const camera = new OrthographicCamera(
-  w / -2,
-  w / 2,
-  h / 2,
-  h / -2,
-  -1000,
-  1000
-);
-const renderer = new WebGLRenderer({ alpha: true });
-const textureLoader = new TextureLoader();
-const loader = new GLTFLoader();
-
-renderer.antialias = true;
-renderer.setSize(w, h);
-renderer.setClearColor(0x000000, 0);
-$container.appendChild(renderer.domElement);
-
-const textureBody = new MeshPhongMaterial({
-  color: 0xcccccc,
-  map: textureLoader.load(textureImage),
-  shininess: 1,
-  flatShading: true,
-});
-
-const hemisphereLight = new HemisphereLight(0x404040, 0xfefefe, 2);
-const spotLight = new SpotLight();
-spotLight.intensity = 1.8;
-spotLight.position.set(400, 2000, 40);
-
-scene.add(hemisphereLight);
-scene.add(spotLight);
-
-let theBody = null;
 const easeOutQuad = (t) => t * (2 - t);
 
-const drawCatBody = (gltf) => {
-  const bodyGeo = gltf.scene.children[0].geometry;
-  bodyGeo.applyMatrix4(new Matrix4().makeTranslation(0, 0, 1));
-  theBody = new Mesh(bodyGeo, textureBody);
-  theBody.position.set(0, 0, 0);
-  theBody.scale.set(140, 140, 140);
-  theBody.rotation.set(0, (currentAngle * Math.PI) / 180, 0);
-  scene.add(theBody);
-};
-
-loader.load(sceneUrl, drawCatBody);
-
-const updateCat = () => {
-  if (!theBody || !mouseXpos) return;
-
-  if (mouseYpos < catCenterYpos + 100) mouseYpos = catCenterYpos;
-  const atan2 = Math.atan2(
-    catCenterYpos - mouseYpos,
-    catCenterXpos - mouseXpos
-  );
-  const targetAngle = (atan2 * -180) / Math.PI - 45;
-
-  if (!isHoverSupport) {
-    if (targetAngle === currentAngle) return;
-    if (!isAnimating) startAngle = currentAngle;
-    let updateAngle = currentAngle;
-
-    if (isAnimating) {
-      updateAngle =
-        startAngle +
-        easeOutQuad(
-          Math.abs(currentAngle - startAngle) /
-            Math.abs(targetAngle - startAngle)
-        ) *
-          (targetAngle - startAngle);
-    }
-
-    if (currentAngle < targetAngle) {
-      currentAngle += ANIMATION_SPEED;
-      isAnimating = true;
-      if (targetAngle < currentAngle) {
-        isAnimating = false;
-        currentAngle = targetAngle;
-      }
-    }
-
-    if (currentAngle > targetAngle) {
-      currentAngle -= ANIMATION_SPEED;
-      isAnimating = true;
-      if (targetAngle > currentAngle) {
-        isAnimating = false;
-        currentAngle = targetAngle;
-      }
-    }
-
-    theBody.rotation.set(0, (updateAngle * Math.PI) / 180, 0);
-  } else {
-    theBody.rotation.set(0, (targetAngle * Math.PI) / 180, 0);
-  }
-};
-
-document.addEventListener("mousemove", (e) => {
-  mouseXpos = e.clientX;
-  mouseYpos = e.clientY;
-});
-
-window.addEventListener("resize", () => {
+class SquareCat extends HTMLElement {
+  isHoverSupport = matchMedia("(hover: hover").matches;
   catCenterXpos = window.innerWidth / 2;
-});
+  catCenterYpos = 280;
+  mouseXpos = false;
+  mouseYpos = false;
+  isAnimating = false;
+  startAngle = 0;
+  currentAngle = 0;
+  theBody = null;
 
-camera.position.set(cameraX + 1, cameraY + 1, cameraZ + 1);
-camera.lookAt(new Vector3(cameraX, cameraY, cameraZ));
-camera.updateProjectionMatrix();
+  constructor() {
+    super();
 
-function animate() {
-  updateCat();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+    this.shadow = this.attachShadow({ mode: "closed" });
+
+    this.w = this.shadow.host.offsetWidth * 2;
+    this.h = this.shadow.host.offsetHeight * 2;
+
+    this.scene = new Scene();
+    this.camera = new OrthographicCamera(
+      this.w / -2,
+      this.w / 2,
+      this.h / 2,
+      this.h / -2,
+      -1000,
+      1000
+    );
+    this.renderer = new WebGLRenderer({ alpha: true });
+    this.textureLoader = new TextureLoader();
+    this.loader = new GLTFLoader();
+
+    this.textureBody = new MeshPhongMaterial({
+      color: 0xcccccc,
+      map: this.textureLoader.load(textureImage),
+      shininess: 1,
+      flatShading: true,
+    });
+
+    this.hemisphereLight = new HemisphereLight(0x404040, 0xfefefe, 2);
+    this.spotLight = new SpotLight();
+
+    this.adjustSize();
+    this.setup();
+    this.animate();
+  }
+
+  adjustSize() {
+    const observer = new ResizeObserver(() => {
+      this.w = this.shadow.host.offsetWidth * 2;
+      this.h = this.shadow.host.offsetHeight * 2;
+      this.camera.left = this.w / -2;
+      this.camera.right = this.w / 2;
+      this.camera.top = this.h / 2;
+      this.camera.bottom = this.h / -2;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(this.w, this.h);
+    });
+    observer.observe(this.shadow.host);
+  }
+
+  setup() {
+    this.renderer.antialias = true;
+    this.renderer.setClearColor(0x000000, 0);
+    this.renderer.setSize(this.w, this.h);
+
+    this.spotLight.intensity = 1.8;
+    this.spotLight.position.set(400, 2000, 40);
+
+    this.scene.add(this.hemisphereLight);
+    this.scene.add(this.spotLight);
+
+    this.camera.position.set(CAMERA_X + 1, CAMERA_Y + 1, CAMERA_Z + 1);
+    this.camera.lookAt(new Vector3(CAMERA_X, CAMERA_Y, CAMERA_Z));
+    this.camera.updateProjectionMatrix();
+
+    document.addEventListener("mousemove", (event) => {
+      this.mouseXpos = event.clientX;
+      this.mouseYpos = event.clientY;
+    });
+
+    window.addEventListener("resize", () => {
+      this.catCenterXpos = window.innerWidth / 2;
+    });
+
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", styles);
+
+    this.shadow.append(linkElem);
+
+    this.loader.load(sceneUrl, this.drawCatBody);
+    this.shadow.append(this.renderer.domElement);
+  }
+
+  drawCatBody = (gltf) => {
+    const bodyGeo = gltf.scene.children[0].geometry;
+    bodyGeo.applyMatrix4(new Matrix4().makeTranslation(0, 0, 1));
+    this.theBody = new Mesh(bodyGeo, this.textureBody);
+    this.theBody.position.set(0, 0, 0);
+    this.theBody.scale.set(140, 140, 140);
+    this.theBody.rotation.set(0, (this.currentAngle * Math.PI) / 180, 0);
+    this.scene.add(this.theBody);
+  };
+
+  updateCat() {
+    if (!this.theBody || !this.mouseXpos) return;
+
+    if (this.mouseYpos < this.catCenterYpos + 100)
+      this.mouseYpos = this.catCenterYpos;
+    const atan2 = Math.atan2(
+      this.catCenterYpos - this.mouseYpos,
+      this.catCenterXpos - this.mouseXpos
+    );
+    const targetAngle = (atan2 * -180) / Math.PI - 45;
+
+    if (!this.isHoverSupport) {
+      if (targetAngle === this.currentAngle) return;
+      if (!this.isAnimating) this.startAngle = currentAngle;
+      let updateAngle = this.currentAngle;
+
+      if (this.isAnimating) {
+        updateAngle =
+          this.startAngle +
+          easeOutQuad(
+            Math.abs(this.currentAngle - this.startAngle) /
+              Math.abs(targetAngle - this.startAngle)
+          ) *
+            (targetAngle - this.startAngle);
+      }
+
+      if (this.currentAngle < targetAngle) {
+        this.currentAngle += ANIMATION_SPEED;
+        this.isAnimating = true;
+        if (targetAngle < this.currentAngle) {
+          this.isAnimating = false;
+          this.currentAngle = targetAngle;
+        }
+      }
+
+      if (currentAngle > targetAngle) {
+        this.currentAngle -= ANIMATION_SPEED;
+        this.isAnimating = true;
+        if (targetAngle > currentAngle) {
+          this.isAnimating = false;
+          this.currentAngle = targetAngle;
+        }
+      }
+
+      this.theBody.rotation.set(0, (updateAngle * Math.PI) / 180, 0);
+    } else {
+      this.theBody.rotation.set(0, (targetAngle * Math.PI) / 180, 0);
+    }
+  }
+
+  animate = () => {
+    this.updateCat();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.animate);
+  };
 }
 
-animate();
+customElements.define("square-cat", SquareCat);
